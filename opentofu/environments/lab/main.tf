5ceac66a4b0a43b4bd21b73c2aac456b2d9183e1
Aditya <adityac1305@gmail.com>
2026-06-07 22:23:12 -0400

    test-pipeline

--- opentofu/environments/lab/main.tf
+++ opentofu/environments/lab/main.tf
@@ -138,6 +138,7 @@ resource "oci_core_default_security_list" "default_sl" {
 ############################################
 # PUBLIC SUBNET
 ############################################
+
 resource "oci_core_subnet" "public_subnet" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.lab_vcn.id
